699804f3352b5a787d781c349ab42a3bf8a05c52
vishal019 <vishalrkjeur019@gmail.com>
2025-10-08 11:19:11 +0530

    random function

--- src/components/Accommodations.tsx
+++ src/components/Accommodations.tsx
@@ -441,7 +441,13 @@ export function Accommodations({
   useEffect(() => {
     setLoading(true);
     fetchAccommodations().then((data) => {
-      setAccommodations(data);
+       const shuffledData = [...data];
+        for (let i = shuffledData.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
+      }
+      setAccommodations(shuffledData);
+      console.log("Fetched accommodations: in random", shuffledData);
       setLoading(false);
     });
   }, []);
