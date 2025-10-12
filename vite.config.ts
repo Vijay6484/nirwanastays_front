95f0ccf8f7210f39567251eb9daf01d1a0576712
Vijay6484 <vt6484@gmail.com>
2025-10-12 12:48:17 +0530

    hjfbaj

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // <-- Add this line
   server: {
-    host: true, // This makes the server accessible on your local network
+    host: true,
   },
   plugins: [react()],
   optimizeDeps: {
